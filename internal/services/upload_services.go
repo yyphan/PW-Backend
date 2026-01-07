8fb63ea4a6490f52120c40550329636c6d8d248b
Yao Yifan <jeromeyao1999@gmail.com>
2026-01-07 22:42:22 +0800

    feat: more readable images

--- internal/services/upload_services.go
+++ internal/services/upload_services.go
@@ -6,9 +6,9 @@ import (
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 const ImgRootFolderKey = "LOCAL_IMAGE_ROOT"
@@ -21,7 +21,11 @@ func UploadImage(file *multipart.FileHeader) (string, error) {
 	defer src.Close()
 
 	ext := filepath.Ext(file.Filename)
-	newFileName := uuid.New().String() + ext
+	rawName := strings.TrimSuffix(filepath.Base(file.Filename), ext)
+
+	cleanName := sanitizeFilename(rawName)
+	timestamp := time.Now().Format("20060102")
+	newFileName := fmt.Sprintf("%s-%s%s", timestamp, cleanName, ext)
 
 	imgRootRela := os.Getenv(ImgRootFolderKey)
 	absRoot, err := filepath.Abs(imgRootRela)
@@ -46,7 +50,23 @@ func UploadImage(file *multipart.FileHeader) (string, error) {
 	if _, err = io.Copy(dst, src); err != nil {
 		return "", err
 	}
-	publicURL := fmt.Sprintf("/images/%d/%02d/%s", now.Year(), now.Month(), newFileName)
+	publicURL := fmt.Sprintf("![%s](/images/%d/%02d/%s)", rawName, now.Year(), now.Month(), newFileName)
 
 	return publicURL, nil
+}
+
+func sanitizeFilename(name string) string {
+	name = strings.ReplaceAll(name, " ", "-")
+	reg := regexp.MustCompile(`[^A-Za-z0-9-_]+`)
+	name = reg.ReplaceAllString(name, "")
+
+	if len(name) > 50 {
+		name = name[:50]
+	}
+
+	if name == "" {
+		name = "image"
+	}
+
+	return name
 }
